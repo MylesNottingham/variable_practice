dbee4d697ab0b7c525a0807a9aaa413db8c6d546
Myles Nottingham <MylesNottingham@gmail.com>
2023-02-22 14:12:42 -0700

    Reassign string variables and add puts statements

--- variables.rb
+++ variables.rb
@@ -2,14 +2,26 @@ first_name = "Myles"
 middle_name = "Avery"
 last_name = "Nottingham"
 
+puts "My full name is %s %s %s" % [first_name, middle_name, last_name]
+
 hours_in_day = 24
 minutes_in_hour = 60
 seconds_in_minute = 60
+
+puts "There are %d hours in a day" % hours_in_day
 
 pi = 3.14
 gpa = 4.0
 ruby_version = 2.7
 
+puts "This is Ruby version %.1f" % ruby_version
+
 going_well = true
 lots_to_learn = true
 give_up = false
+
+first_name = "Neil"
+middle_name = "Patrick"
+last_name = "Harris"
+
+puts "Now I have become %s %s %s" % [first_name, middle_name, last_name]
